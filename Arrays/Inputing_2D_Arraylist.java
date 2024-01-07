18ff2d4fd4ac2df14aca9d32c7d0d4780ca085cd
Abhinav <abhinavsingh12347@gmail.com>
2024-01-07 14:35:51 +0530

    Added arraylist 2d

---
+++ Arrays/Inputing_2D_Arraylist.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Inputing_2D_Arraylist {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        // intializing the 2D arraylist
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+        }
+        // inputing the 2D arraylist
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add(sc.nextInt());
+            }
+        }
+        // printing the 2D arraylist
+        for (int i = 0; i < 3; i++) {
+            list.get(i);
+        }
+        System.out.println(list);
+    }
+}
