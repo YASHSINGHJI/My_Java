3ad1069435aa7d1ead67af1d652efad0c41f0a48
Abhinav <abhinavsingh12347@gmail.com>
2024-01-28 19:46:14 +0530

    Added prime no

--- Java Basics/Prime_No_By_FunctionMethod.java
+++ Java Basics/Prime_No_By_FunctionMethod.java
@@ -2,25 +2,18 @@ import java.util.Scanner;
 
 public class Prime_No_By_FunctionMethod {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(isPrime(n));
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        isPrime(n);
     }
-
-    static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        int c = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                c++;
+    static int isPrime(int n){
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                return 1;
+            }
+            else{
+                return 0;
             }
         }
-
-        if (c == 2)
-            return true;
-        else
-            return false;
-    }
+            }
 }
