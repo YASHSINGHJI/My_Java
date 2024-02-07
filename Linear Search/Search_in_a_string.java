3129e190347f22c806c378a0e671af92fca7846e
Abhinav <abhinavsingh12347@gmail.com>
2024-02-07 21:45:07 +0530

    added search in string

---
+++ Linear Search/Search_in_a_string.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Search_in_a_string {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String str = sc.nextLine();
+        System.out.println("Enter the character to be searched: ");
+        char target = sc.next().charAt(0);
+        System.out.println(search(str, target));
+    }
+
+    static boolean search(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
