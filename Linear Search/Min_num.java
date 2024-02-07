05b79a93e7c5f3bd422e83138762480de4d0d484
Abhinav <abhinavsingh12347@gmail.com>
2024-02-07 21:55:52 +0530

    added min num

---
+++ Linear Search/Min_num.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Min_num {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The minimum number is: " + min(arr));
+    }
+
+    static int min(int[] arr) {
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
