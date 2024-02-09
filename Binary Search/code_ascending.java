c10c836332e3d4ef4337fe208f28ff89f00bb5e2
Abhinav <abhinavsingh12347@gmail.com>
2024-02-09 20:44:53 +0530

    added hfhyf

--- Binary Search/code_ascending.java
+++ Binary Search/code_ascending.java
@@ -1,7 +1,7 @@
 public class code_ascending {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        BinarySearch(arr, 9);
+        BinarySearch(arr, 5);
     }
 
     static void BinarySearch(int[] arr, int target) {
