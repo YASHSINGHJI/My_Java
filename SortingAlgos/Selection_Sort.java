ddba0eded6271f1f814235f5fb730916848d7fd9
Abhinav <abhinavsingh12347@gmail.com>
2024-03-26 17:04:33 +0530

    added selection sort

--- SortingAlgos/Selection_Sort.java
+++ SortingAlgos/Selection_Sort.java
@@ -3,28 +3,24 @@ package SortingAlgos;
 public class Selection_Sort {
     public static void main(String[] args) {
         int arr[] = { 3, 4, 2, 5, 1 };
-        for (int i = 0; i < arr.length; i++) {
-            int max = max(arr, 0, arr.length - i - 1);
-            swap(arr, max, arr.length - i - 1);
-        }
+        selectionSort(arr);
         for (int i : arr) {
             System.out.print(i + " ");
         }
     }
 
-    static int max(int arr[], int start, int end) {
-        int max = start;
+    static void selectionSort(int arr[]) {
         int n = arr.length;
-        for (int i = start; i <= end; i++) {
-            if (arr[i] > arr[max])
-                max = i;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
-        return max;
-    }
-
-    static void swap(int arr[], int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
     }
 }
