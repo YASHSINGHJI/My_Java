61453e21ea9db523e4f0ea5184e73d39b5b26b20
Abhinav <abhinavsingh12347@gmail.com>
2024-03-28 18:26:44 +0530

    added insertion sort algo

---
+++ SortingAlgos/insertion_sort.java
@@ -0,0 +1,32 @@
+package SortingAlgos;
+
+public class insertion_sort {
+    public static void main(String[] args) {
+        int arr[] = { 12, 11, 13, 5, 6 };
+        insertionSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+
+            }
+
+        }
+    }
+
+}
+
+/*
+ * insertion sort means taking the elements and
+ * comparing to LHS and putting them in correct order
+ */
