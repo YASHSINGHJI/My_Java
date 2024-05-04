8634c4a585199109d64277790529cee4d02ccbd2
Abhinav <abhinavsingh12347@gmail.com>
2024-05-04 11:20:06 +0530

    added cyclic sort

---
+++ SortingAlgos/Cyclic_Sort.java
@@ -0,0 +1,27 @@
+package SortingAlgos;
+
+public class Cyclic_Sort {
+    public static void main(String[] args) {
+        int[] arr = { 6, 5, 4, 3, 2, 1 };
+        cyclicSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    static void cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] - 1 != i) {
+                int temp = arr[arr[i] - 1];
+                arr[arr[i] - 1] = arr[i];
+                arr[i] = temp;
+            } else {
+                i++;
+
+            }
+
+        }
+
+    }
+}
