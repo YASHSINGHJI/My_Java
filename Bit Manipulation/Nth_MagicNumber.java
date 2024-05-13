b0a9ecfe089563cc293ec1e11115306e5e95e12d
Abhinav <abhinavsingh12347@gmail.com>
2024-05-13 12:21:36 +0530

    nth_magic number

---
+++ Bit Manipulation/Nth_MagicNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Nth_MagicNumber {
+    public static void main(String[] args) {
+        System.out.println("Enter the value of n: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(nthMagicNumber(n));
+    }
+
+    static int nthMagicNumber(int n) {
+        int ans = 0;
+        int pow = 1;
+        while (n != 0) {
+            pow = pow * 5;
+            if ((n & 1) == 1) {
+                ans += pow;
+            }
+            n = n >> 1;
+        }
+        return ans;
+    }
+}
