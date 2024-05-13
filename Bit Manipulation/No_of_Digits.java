6537db9145c36276128a89a570964037f4ef558b
Abhinav <abhinavsingh12347@gmail.com>
2024-05-13 13:12:43 +0530

    count no of bit in ana integer

---
+++ Bit Manipulation/No_of_Digits.java
@@ -0,0 +1,8 @@
+public class No_of_Digits {
+    public static void main(String[] args) {
+        int n = 10;
+        int base = 2;
+        System.out.println((int) (Math.log(n) / Math.log(base) + 1));
+    }
+
+}
