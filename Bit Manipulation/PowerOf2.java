f2dd6360a72659ab564ff9e528cd7872f69e0921
Abhinav <abhinavsingh12347@gmail.com>
2024-05-13 13:39:41 +0530

    added comments

--- Bit Manipulation/PowerOf2.java
+++ Bit Manipulation/PowerOf2.java
@@ -14,3 +14,10 @@ public class PowerOf2 {
         }
     }
 }
+
+/*
+ * For example suppose we have 10000000 in binary form
+ * and we subtract 1 from it we get 01111111.
+ * Now if we do bitwise & of these two numbers we get 0.
+ * 
+ */
