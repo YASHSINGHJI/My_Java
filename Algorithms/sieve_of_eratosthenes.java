2b000df3d309606b22bd2236ad051b7315bf0a4a
Abhinav <abhinavsingh12347@gmail.com>
2024-05-13 19:36:16 +0530

    sieve of eratosthenes algo

---
+++ Algorithms/sieve_of_eratosthenes.java
@@ -0,0 +1,33 @@
+package Algorithms;
+
+/**
+ * sieve_of_eratosthenes
+ */
+public class sieve_of_eratosthenes {
+    public static void main(String[] args) {
+        int n = 40;
+        boolean[] prime = new boolean[n + 1];
+        sieve(n, prime);
+
+    }
+
+    static void sieve(int n, boolean[] prime) {
+        for (int i = 2; i * i <= n; i++) {
+            if (!prime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    prime[j] = true;
+                }
+
+            }
+
+        }
+        for (int i = 2; i <= n; i++) {
+            if (!prime[i]) {
+                System.out.print(i + " ");
+
+            }
+
+        }
+
+    }
+}
