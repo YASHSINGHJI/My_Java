cfc6a3551b039a4ffe1774b6e44d7bba0988a0f2
Abhinav <abhinavsingh12347@gmail.com>
2024-05-18 19:32:50 +0530

    commit 1

--- Recursion/BubbleSortRecursion.java
+++ Recursion/BubbleSortRecursion.java
@@ -5,24 +5,24 @@ import java.util.Arrays;
 public class BubbleSortRecursion {
     public static void main(String[] args) {
         int[] arr = { 2, 3, 0, 1 };
-        pattern(arr, arr.length - 1, 0);
+        BubbleSort(arr, arr.length - 1, 0);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void pattern(int[] arr, int end, int start) {
+    static void BubbleSort(int[] arr, int end, int start) {
         if (end == 0)
             return;
         if (start == end) {
-            pattern(arr, end - 1, 0);
+            BubbleSort(arr, end - 1, 0);
         }
         if (start + 1 <= arr.length - 1 && arr[start] >= arr[start + 1]) {
             int temp = arr[start];
             arr[start] = arr[start + 1];
             arr[start + 1] = temp;
-            pattern(arr, end, start + 1);
+            BubbleSort(arr, end, start + 1);
         }
         if (start + 1 <= arr.length - 1 && arr[start] < arr[start + 1])
-            pattern(arr, end, start + 1);
+            BubbleSort(arr, end, start + 1);
 
     }
 }
