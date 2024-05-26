a37809a841559f9ba5d0e24aac5243e7ef40f141
Abhinav <abhinavsingh12347@gmail.com>
2024-05-26 11:15:16 +0530

    added maze problem

---
+++ Recursion/MazeProblem.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class MazeProblem {
+    public static void main(String[] args) {
+        maze("", 3, 3);
+    }
+
+    static void maze(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(p);
+            return;
+        }
+        if (r > 1) {
+            maze(p + "D", r - 1, c);
+        }
+        if (c > 1) {
+            maze(p + "R", r, c - 1);
+        }
+    }
+}
