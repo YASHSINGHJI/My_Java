d4d816d8e92dcd9b4e2ccacfa802749daf5a76d9
Abhinav <abhinavsingh12347@gmail.com>
2024-05-31 18:49:34 +0530

    constructor overload added

--- OOPs/Create_Class.java
+++ OOPs/Create_Class.java
@@ -6,12 +6,22 @@ public class Create_Class {
             int rno;
             int marks;
             String name;
+
+            Student() {
+                System.out.println("Default Constructor called");
+            }
+
+            Student(int rno, int marks, String name) {
+                this.rno = rno;
+                this.marks = marks;
+                this.name = name;
+                System.out.println("Parameterized Constructor called");
+            }
         }
 
-        Student s = new Student();
-        s.rno = 12;
-        s.marks = 1000;
-        s.name = "Rahul";
-        // Alternatively, Student s=new Student(12,1000,"Rahul");
+        Student s = new Student(12, 100, "Rahul");
+        Student s1 = new Student();
+        // this keyword is used to refer to the current object. It is simply replaced by
+        // the object name.
     }
 }
