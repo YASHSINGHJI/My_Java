56ae8fd0333c4221c57e16a4f53e49e4b60549c5
Abhinav <abhinavsingh12347@gmail.com>
2024-07-12 16:58:14 +0530

    transversals

---
+++ Trees/Traversal.java
@@ -0,0 +1,34 @@
+package Trees;
+
+import Trees.BST.Node;
+
+public class Traversal {
+    public void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.data + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+
+    }
+
+    public void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.data + " ");
+
+    }
+}
